a63f311d224bc0e63ede701f94a313ce88b75453
Liya <lychen86@126.com>
2018-02-05 17:42:32 +0800

    Initial Commit

---
+++ LYMethodChaining/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYMethodChaining
+//
+//  Created by Liya on 2018/2/5.
+//  Copyright © 2018年 Liya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYMethodChaining/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYMethodChaining
+//
+//  Created by Liya on 2018/2/5.
+//  Copyright © 2018年 Liya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
