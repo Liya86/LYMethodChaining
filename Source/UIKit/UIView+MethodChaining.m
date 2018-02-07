78ebbc5ed4ce16034f9a9c6e39bbf77db664f799
Liya86 <289462528@qq.com>
2018-02-07 15:43:04 +0800

    补充一些UIView的常用方法对应的链式调用

--- Source/UIKit/UIView+MethodChaining.m
+++ Source/UIKit/UIView+MethodChaining.m
@@ -289,4 +289,188 @@
         return self;
     };
 }
+
+- (UIView * (^)(UIColor *))mc_tintColor {
+    MC_Weakify(self) return ^UIView *(UIColor *tintColor) {
+        MC_Strongify(self);
+        self.tintColor = tintColor;
+        return self;
+    };
+}
+
+- (UIView * (^)(UIViewTintAdjustmentMode))mc_tintAdjustmentMode {
+    MC_Weakify(self) return ^UIView *(UIViewTintAdjustmentMode tintAdjustmentMode) {
+        MC_Strongify(self);
+        self.tintAdjustmentMode = tintAdjustmentMode;
+        return self;
+    };
+}
+
+- (UIView * (^)(void))mc_tintColorDidChange {
+    MC_Weakify(self) return ^UIView *(void) {
+        MC_Strongify(self);
+        [self tintColorDidChange];
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *))mc_addSubView {
+    MC_Weakify(self) return ^UIView *(UIView *subView) {
+        MC_Strongify(self);
+        if (subView) {
+            [self addSubview:subView];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *, NSInteger))mc_insertSubView {
+    MC_Weakify(self) return ^UIView *(UIView *subView, NSInteger index) {
+        MC_Strongify(self);
+        if (subView) {
+            [self insertSubview:subView atIndex:index];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *, UIView *))mc_insertSubViewAboveSubview {
+    MC_Weakify(self) return ^UIView *(UIView *subView, UIView *siblingSubview) {
+        MC_Strongify(self);
+        if (subView) {
+            [self insertSubview:subView aboveSubview:siblingSubview];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *, UIView *))mc_insertSubViewBelowSubview {
+    MC_Weakify(self) return ^UIView *(UIView *subView, UIView *siblingSubview) {
+        MC_Strongify(self);
+        if (subView) {
+            [self insertSubview:subView belowSubview:siblingSubview];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(void))mc_removeAllSubViews {
+    MC_Weakify(self) return ^UIView *(void) {
+        MC_Strongify(self);
+        NSArray *subViews = self.subviews;
+        subViews.mc_enumerateObjectsUsingBlock(^(UIView *subView, NSUInteger index, BOOL *stop) {
+            [subView removeFromSuperview];
+        });
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *))mc_removeSubView {
+    MC_Weakify(self) return ^UIView *(UIView *subView) {
+        MC_Strongify(self);
+        if (subView.superview == self) {
+            [subView removeFromSuperview];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *))mc_bringSubviewToFront {
+    MC_Weakify(self) return ^UIView *(UIView *subView) {
+        MC_Strongify(self);
+        if (subView) {
+            [self bringSubviewToFront:subView];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *))mc_sendSubviewToBack {
+    MC_Weakify(self) return ^UIView *(UIView *subView) {
+        MC_Strongify(self);
+        if (subView) {
+            [self sendSubviewToBack:subView];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(void))mc_removeFromSuperview {
+    MC_Weakify(self) return ^UIView *(void) {
+        MC_Strongify(self);
+        [self removeFromSuperview];
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *))mc_addToSuperView {
+    MC_Weakify(self) return ^UIView *(UIView *superView) {
+        MC_Strongify(self);
+        if (superView) {
+            [superView addSubview:self];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *, NSInteger))mc_insertToSuperView {
+    MC_Weakify(self) return ^UIView *(UIView *superView, NSInteger index) {
+        MC_Strongify(self);
+        if (superView) {
+            [superView insertSubview:self atIndex:index];
+        } else {
+            [self.superview insertSubview:self atIndex:index];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *, UIView *))mc_insertToSuperViewAboveSubview {
+    MC_Weakify(self) return ^UIView *(UIView *superView, UIView *siblingSubview) {
+        MC_Strongify(self);
+        if (superView) {
+            [superView insertSubview:self aboveSubview:siblingSubview];
+        } else {
+            [self.superview insertSubview:self aboveSubview:siblingSubview];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *, UIView *))mc_insertToSuperViewBelowSubview {
+    MC_Weakify(self) return ^UIView *(UIView *superView, UIView *siblingSubview) {
+        MC_Strongify(self);
+        if (superView) {
+            [superView insertSubview:self belowSubview:siblingSubview];
+        } else {
+            [self.superview insertSubview:self belowSubview:siblingSubview];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *))mc_bringToSuperViewFront {
+    MC_Weakify(self) return ^UIView *(UIView *superView) {
+        MC_Strongify(self);
+        if (superView) {
+            [superView bringSubviewToFront:self];
+        } else {
+            [self.superview bringSubviewToFront:self];
+        }
+        return self;
+    };
+}
+
+- (UIView * (^)(UIView *))mc_sendToSuperViewBack {
+    MC_Weakify(self) return ^UIView *(UIView *superView) {
+        MC_Strongify(self);
+        if (superView) {
+            [superView sendSubviewToBack:self];
+        } else {
+            [self.superview bringSubviewToFront:self];
+        }
+        return self;
+    };
+}
+
 @end
